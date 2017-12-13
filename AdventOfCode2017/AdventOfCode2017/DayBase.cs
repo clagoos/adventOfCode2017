da3ebde3cdab6f9fd87ae8ad3aa9bc5a1c4fe2fd
Lech Osiński <osinski.lech@gmail.com>
2017-12-13 13:07:36 +0100

    day 1 solution + some infrastructure for executing code for each day riddle

---
+++ AdventOfCode2017/AdventOfCode2017/DayBase.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode2017
+{
+    public abstract class DayBase
+    {
+        public string ClassName => GetType().Name;
+        public abstract string Calculate();
+    }
+}
--- AdventOfCode2017/AdventOfCode2017/Program.cs
+++ AdventOfCode2017/AdventOfCode2017/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace AdventOfCode2017
+﻿using System;
+
+namespace AdventOfCode2017
 {
     public class Program
     {
         public static void Main()
         {
+            ExecuteDay(new Day1());
+        }
+
+        public static void ExecuteDay(DayBase day)
+        {
+            try
+            {
+                Console.WriteLine($"{day.ClassName} COMMENCING");
+                Console.WriteLine($"Result: {day.Calculate()}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exeption of type {e.GetType()} occured.");
+                Console.WriteLine($"\tMessage starts with: {e.Message.Substring(0, e.Message.Length < 120 ? e.Message.Length : 120)}");
+            }
+            finally
+            {
+                Console.WriteLine($"END OF {day.ClassName}");
+                Console.WriteLine();
+            }
         }
     }
 }
