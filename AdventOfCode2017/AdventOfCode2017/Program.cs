44a1c8482ee5edbf2b0c39161350ad4971364199
Lech Osiński <osinski.lech@gmail.com>
2017-12-13 12:38:03 +0100

    setting up the project + gitignore

---
+++ AdventOfCode2017/AdventOfCode2017/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode2017
+{
+    public class Program
+    {
+        public static void Main()
+        {
+        }
+    }
+}
---
+++ AdventOfCode2017/AdventOfCode2017/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AdventOfCode2017")]
+[assembly: AssemblyDescription("Code for solving Advent of Code 2017 puzzles")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Lech Osiński")]
+[assembly: AssemblyProduct("AdventOfCode2017")]
+[assembly: AssemblyCopyright("Copyright © Lech Osiński 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4646c178-8cf0-473e-b142-f4bdb895094a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
